1c0d8f90923c47e064e2b7a5db1c2a9ff0dc7674
Erik Hansen <e.hansen31@live.com>
2020-03-07 23:17:41 -0500

    finish car dealership problem

--- LearnErlang/src/car_dealership.erl
+++ LearnErlang/src/car_dealership.erl
@@ -2,14 +2,24 @@
 
 -export([listPrices/1]).
 
+listPrices(Currency) ->
+    Cars = ["BMW i8", "Lamborghini Huracan", "Ferrari f12"],
+    listPrices(Cars, Currency).
 
-listPrices(Currency) -> 
-    Cars = #{"BMW i8"=>150000, "Lamborghini Huracan"=>500000, "Ferrari f12"=> 700000},
-
-listPrices([], Currency)->
-        io:fwrite("Price" ++ integer_to_list(ConvertedPrice) ++ "\n");
-
-listPrices([], Currency)->
-    .
-
-round(number) -> null.
+listPrices([Car | Rest], Currency) ->
+    CarPrices = #{"BMW i8" => 150000,
+		  "Lamborghini Huracan" => 500000,
+		  "Ferrari f12" => 700000},
+    case Currency of
+      eur ->
+	  ConvertedPrice = round(maps:get(Car, CarPrices) *
+				   8.9e-1);
+      gdp ->
+	  ConvertedPrice = round(maps:get(Car, CarPrices) *
+				   7.7e-1);
+      usd -> ConvertedPrice = maps:get(Car, CarPrices)
+    end,
+    io:fwrite("Price " ++
+		integer_to_list(ConvertedPrice) ++ "\n"),
+    listPrices(Rest, Currency);
+listPrices([], _Currency) -> ok.
